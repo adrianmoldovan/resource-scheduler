8f2dbe0085a3ac9c202a576ec3ea909259b7b13f
adimoldovan <adrian.moldovan00@gmail.com>
2015-05-14 21:03:23 +0300

    initial import; maven project setup;

---
+++ src/main/java/com/adm/scheduler/Gateway.java
@@ -0,0 +1,5 @@
+package com.adm.scheduler;
+
+public interface Gateway {
+    public void send(Message msg);
+}
---
+++ src/main/java/com/adm/scheduler/Message.java
@@ -0,0 +1,5 @@
+package com.adm.scheduler;
+
+public interface Message {
+    public void completed();
+}
